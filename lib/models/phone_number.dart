d05409831ea39366c8d0d27a53be6ab959bf32e8
Edenik <Edenik5@gmail.com>
2020-10-15 21:45:51 +0300

    Phone Number Model

---
+++ lib/models/phone_number.dart
@@ -0,0 +1,33 @@
+class PhoneNumber {
+  final String prefix;
+  final String line;
+  final String pattern = r'^((\+|00)?972\-?|0)(([23489]|[57]\d)\-?\d{7})$';
+
+  PhoneNumber({this.prefix, this.line});
+
+  bool isValidIsraeliPhoneNumber() {
+    final regExp = RegExp(pattern);
+    print(line);
+    if (line == null || line.isEmpty) {
+      return false;
+    }
+
+    if (!regExp.hasMatch(line)) {
+      return false;
+    }
+    return true;
+  }
+
+  String get phoneNumberWithPrefix {
+    String num;
+    if (line.contains('+972')) {
+      num = line.substring(4);
+    } else if (line.contains('972')) {
+      num = line.substring(3);
+    } else {
+      num = line.substring(1);
+    }
+    final String phoneNumber = '+' + prefix + num;
+    return phoneNumber;
+  }
+}
